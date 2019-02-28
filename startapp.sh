d952abca30522106bb86e3c871333923265931e9
Chetan Conikee <conikee@gmail.com>
2019-02-28 11:43:39 -0800

    updated scripts

--- startapp.sh
+++ startapp.sh
@@ -1,5 +1,6 @@
 SL_FEATURE_FLAG_ENABLED_HTTP=true sl run -- java -Dshiftleft.sec.xxe=EXTERNAL -Dshiftleft.sec.mode=BLOCK -jar target/helloshiftleft-play-assembly-0.0.1-SNAPSHOT.jar &
-sleep 30
+
+sleep 100
 
 for i in {1..100}
 do
