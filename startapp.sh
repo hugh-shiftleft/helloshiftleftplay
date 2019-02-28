c0dfaf61d3b3a46a1fab0111fedd15012c57f877
Chetan Conikee <conikee@gmail.com>
2019-02-28 11:37:27 -0800

    updated scripts

--- startapp.sh
+++ startapp.sh
@@ -1,4 +1,49 @@
 SL_FEATURE_FLAG_ENABLED_HTTP=true sl run -- java -Dshiftleft.sec.xxe=EXTERNAL -Dshiftleft.sec.mode=BLOCK -jar target/helloshiftleft-play-assembly-0.0.1-SNAPSHOT.jar &
 sleep 30
 
+for i in {1..100}
+do
+    curl http://localhost:9000/customers/1
+    curl http://localhost:9000/customers/
+    curl http://localhost:9000/customers/2
+    curl http://localhost:9000/customers/
+    curl http://localhost:9000/customers/1
+    curl http://localhost:9000/customers/1
+    curl http://localhost:9000/customers/1
+    curl http://localhost:9000/customers/1
+    sleep 30
+done
+sleep 10
+for i in {1..50}
+do
+    curl "http://localhost:9000/rawcustomersbyname/Joe'%20or%20'1'='1"
+done
+sleep 10
+for i in {1..50}
+do
+    curl "http://localhost:9000/checkAccount/admin/Joe'%20or%20'1'='1"
+done
+sleep 10
+for i in {1..100}
+do
+    curl http://localhost:9000/account
+    curl http://localhost:9000/account/1
+    curl http://localhost:9000/account/2
+    curl http://localhost:9000/account/3
+    curl http://localhost:9000/account/4
+    curl http://localhost:9000/account/5
+    curl http://localhost:9000/account/1
+    sleep 30
+done
+sleep 5
+for i in {1..50}
+do
+    curl http://localhost:9000/patients
+    sleep 30
+done
+sleep 10
+for i in {1..50}
+do
+    curl -X GET --header 'Content-Type: application/xml' --data '<!--?xml version="1.0"?--><!DOCTYPE replace [<!ENTITY ent SYSTEM "file:///etc/passwd">]><userInfo><firstName>Joe</firstName><name>&ent;</name></userInfo>' http://localhost:9000/customersXML
+done
 
