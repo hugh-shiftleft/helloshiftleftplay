899a276ab3d84a9a8478079ec917bad5f3701929
Chetan Conikee <conikee@gmail.com>
2019-03-03 10:51:35 -0800

    Update startapp.sh

--- startapp.sh
+++ startapp.sh
@@ -12,20 +12,33 @@ do
     curl http://localhost:9000/customers/1
     curl http://localhost:9000/customers/1
     curl http://localhost:9000/customers/1
-    sleep 30
+    sleep 60
 done
 sleep 10
-for i in {1..50}
+for i in {1..200}
+do
+    curl http://localhost:9000/customers/1
+    curl http://localhost:9000/customers/
+    curl http://localhost:9000/customers/2
+    curl http://localhost:9000/customers/
+    curl http://localhost:9000/account/3
+    curl http://localhost:9000/account/4
+    curl http://localhost:9000/account/5
+    curl http://localhost:9000/account/1
+    sleep 60
+done
+sleep 10
+for i in {1..150}
 do
     curl "http://localhost:9000/rawcustomersbyname/Joe'%20or%20'1'='1"
 done
 sleep 10
-for i in {1..50}
+for i in {1..150}
 do
     curl "http://localhost:9000/checkAccount/admin/Joe'%20or%20'1'='1"
 done
 sleep 10
-for i in {1..100}
+for i in {1..200}
 do
     curl http://localhost:9000/account
     curl http://localhost:9000/account/1
@@ -34,17 +47,44 @@ do
     curl http://localhost:9000/account/4
     curl http://localhost:9000/account/5
     curl http://localhost:9000/account/1
-    sleep 30
+    sleep 60
 done
 sleep 5
-for i in {1..50}
+for i in {1..150}
 do
     curl http://localhost:9000/patients
     sleep 30
+done
+sleep 10
+for i in {1..200}
+do
+    curl http://localhost:9000/customers/1
+    curl http://localhost:9000/customers/
+    curl http://localhost:9000/customers/2
+    curl http://localhost:9000/customers/
+    curl http://localhost:9000/account/3
+    curl http://localhost:9000/account/4
+    curl http://localhost:9000/account/5
+    curl http://localhost:9000/account/1
+    sleep 60
 done
 sleep 10
 for i in {1..50}
 do
     curl -X GET --header 'Content-Type: application/xml' --data '<!--?xml version="1.0"?--><!DOCTYPE replace [<!ENTITY ent SYSTEM "file:///etc/passwd">]><userInfo><firstName>Joe</firstName><name>&ent;</name></userInfo>' http://localhost:9000/customersXML
 done
+sleep 20
+for i in {1..200}
+do
+    curl http://localhost:9000/customers/1
+    curl http://localhost:9000/customers/
+    curl http://localhost:9000/customers/2
+    curl http://localhost:9000/customers/
+    curl http://localhost:9000/account/3
+    curl http://localhost:9000/account/4
+    curl http://localhost:9000/account/5
+    curl http://localhost:9000/account/1
+    sleep 60
+done
+sleep 10
 
