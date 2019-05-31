6627aee707ffd19f4115cffc0575ccb40fe30ae0
hugh-shiftleft <49698586+hugh-shiftleft@users.noreply.github.com>
2019-05-31 15:11:01 -0400

    Update startapp.sh

--- startapp.sh
+++ startapp.sh
@@ -1,4 +1,4 @@
-SL_FEATURE_FLAG_ENABLED_HTTP=true sl run -- java -Dshiftleft.sec.xxe=EXTERNAL -Dshiftleft.sec.mode=BLOCK -jar target/helloshiftleft-play-assembly-0.0.1-SNAPSHOT.jar &
+SHIFTLEFT_SEC_COLLECT_ATTACK_INFO=true sl run -- java -Dshiftleft.sec.xxe=EXTERNAL -Dshiftleft.sec.mode=BLOCK -jar target/helloshiftleft-play-assembly-0.0.1-SNAPSHOT.jar &
 
 sleep 100
 
